49dc47e649a31a01c5fe39650b62b3fe832fbd21
chandan <chandanchethan321@gmail.com>
2023-04-30 18:03:24 +0530

    added changes

--- Components/Homgrid.js
+++ Components/Homgrid.js
@@ -18,23 +18,28 @@ export const Homgrid = () => {
   }
 
   return (
-    <div className={Styles.gridcont}>
-      <div className={Styles.form}>
-        <div className={Styles.flexfont}>
-          <div className={Styles.formtxt}>Welcome to a platform where you can share text or code!</div>
-          <div className={Styles.text2}>Share and collaborate on code using simple links</div>
+    <div className={Styles.container}>
+      <div className={Styles.contgrid}>
+        <div className={Styles.form}>
+          <div className={Styles.flexfont}>
+            <div className={Styles.formtxt}>Welcome to a platform where you can share text or code!</div>
+            <div className={Styles.text2}>Share and collaborate on code using simple links</div>
+          </div>
+          <div className={Styles.inp}>
+            <div className={Styles.inpdiv}>
+              <input className={Styles.input} onChange={(ev) => setname(ev.target.value)} type="text" placeholder="Name" />
+              <input className={Styles.input} onChange={(ev) => setroom(ev.target.value)} type="text" placeholder="Code-id" />
+            </div>
+            <button
+              className={`${Styles.btn} ${room && name ? Styles.whiteBtn : ''}`}
+              type="button" onClick={Btnclick}>
+              Send
+            </button>
+
+          </div>
         </div>
-        <div className={Styles.inp}>
-          <input className={Styles.input} onChange={(ev) => setname(ev.target.value)} type="text" placeholder="Name" />
-          <input className={Styles.input} onChange={(ev) => setroom(ev.target.value)} type="text" placeholder="Code-id" />
-          <button
-            className={`${Styles.btn} ${room && name ? Styles.whiteBtn : ''}`}
-            type="button" onClick={Btnclick}>
-            Send
-          </button>
-        </div>
+        <div className={Styles.imgbox}>< img src='codd.png' className={Styles.img} alt="codeimg" /></div>
       </div>
-      <div className={Styles.imgbox}>< img src='code.png' className={Styles.img} alt="codeimg" /></div>
     </div>
   )
 }
--- Components/Navbar.js
+++ Components/Navbar.js
@@ -4,12 +4,17 @@ export const Navbar = () => {
     return (
 
         <div className={Styles.container}>
-            <div className={Styles.logo}><img src='coding-folder.png' className={Styles.img} /><div className={Styles.name}>Sharecode.com</div></div>
-            <div className={Styles.list}>
-                <div>Home</div>
-                <div>Features</div>
-                <div>Help</div>
-                <div>Contact</div>
+            <div className={Styles.contflex}>
+                <div className={Styles.logo}>
+                    <img src='coding-folder.png' className={Styles.img} />
+                    <div className={Styles.name}>Sharecode.com</div>
+                </div>
+                <div className={Styles.list}>
+                    <div>Home</div>
+                    <div>Features</div>
+                    <div>Help</div>
+                    <div>Contact</div>
+                </div>
             </div>
 
         </div>
