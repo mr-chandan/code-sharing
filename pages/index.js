7e0c25929bfbacbe187e66b86a8b6293cf9e894e
chandan <chandanchethan321@gmail.com>
2023-04-30 12:57:21 +0530

    fiot

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,7 @@ import Head from 'next/head'
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Home Page</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+
 
       <Navbar />
       <Homgrid />
