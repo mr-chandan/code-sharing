abf098819a6d50027185274511f3e86ca1ba31ff
chandan <chandanchethan321@gmail.com>
2023-04-30 12:20:17 +0530

    title

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
-import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return <>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    </Head>
 
     <Component {...pageProps} />
     <Analytics />
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import { Navbar } from '@/Components/Navbar'
-import  {Homgrid}  from '@/Components/Homgrid'
-
+import { Homgrid } from '@/Components/Homgrid'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      <Homgrid/>
+      <Head>
+      <title>Home Page</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      </Head>
+
+      <Navbar />
+      <Homgrid />
     </>
   )
 }
