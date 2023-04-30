a0df0a6a656112db9c8ae99994d05349918c6843
chandan <chandanchethan321@gmail.com>
2023-04-30 12:11:52 +0530

    head

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '@/styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return <>
-  <Component {...pageProps} />
-  <Analytics />
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    </Head>
+
+    <Component {...pageProps} />
+    <Analytics />
   </>
 }
