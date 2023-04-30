a815f4fad984d7d06bc6e48343e888b2d28efc32
chandan <chandanchethan321@gmail.com>
2023-04-30 12:59:09 +0530

    res

--- pages/_app.js
+++ pages/_app.js
@@ -7,9 +7,6 @@ export default function App({ Component, pageProps }) {
       <title>Home Page</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
-    <Head>
-    <meta name="viewport" content="viewport-fit=cover" />
-    </Head>
     <Component {...pageProps} />
   </>
 }
